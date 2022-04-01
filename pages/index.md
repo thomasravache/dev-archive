91cd3375921c999c1ab6abb8229a6f890a0ef9c9
Thomas Ravache <thomasravache31@gmail.com>
2022-04-01 15:58:55 -0300

    feat: index introduction

---
+++ pages/index.md
@@ -0,0 +1,10 @@
+# Introdução
+
+*Inspirado pelo **[second brain](https://segundocerebro.vercel.app/).*** Feito com [Nextra](https://github.com/shuding/nextra).
+
+Este projeto foi feito para:
+
+- Anotações
+- Dicas
+- E outros afins sobre código
+
--- theme.config.js
+++ theme.config.js
@@ -6,7 +6,7 @@ export default {
     <>
       <span className="mr-2 font-extrabold hidden md:inline">Thomas Dev Archive</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        Para anotações, dicas e boas práticas relacionadas a código e +
+        Para anotações, dicas, boas práticas relacionadas a código e +
       </span>
     </>
   ),
